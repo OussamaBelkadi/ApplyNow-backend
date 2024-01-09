c86e4c26adbc002bcf124d9f02200ac689575d97
kalimu7 <mahjourkarim@gmail.com>
2024-01-09 11:03:39 +0100

    stable version

--- src/main/java/example/brief/MyRh/services/CandidatService.java
+++
@@ -1,9 +0,0 @@
-package example.brief.MyRh.services;
-
-import example.brief.MyRh.dtos.CandiatDto;
-
-public interface CandidatService {
-
-    CandiatDto registerCandiat(CandiatDto candiatDto);
-
-}
